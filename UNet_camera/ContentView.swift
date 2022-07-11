e5da05b1736bad794583ef834e26b4f2b35caf36
hongzzi-zzi <ahnhe9227@naver.com1971078@ewhain.net>
2022-07-11 14:44:04 +0900

    Initial Commit

---
+++ UNet_camera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UNet_camera
+//
+//  Created by 안홍은 on 2022/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UNet_camera/UNet_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  UNet_cameraApp.swift
+//  UNet_camera
+//
+//  Created by 안홍은 on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct UNet_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
